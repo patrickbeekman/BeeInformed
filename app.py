96d3fea503484f2b964ef4a516e542014e4823e5
patrickbeekman <patrickbeekman1095@gmail.com>
2019-09-15 20:18:02 -0400

    refactored the map creation.

--- app.py
+++ app.py
@@ -1,9 +1,13 @@
 from flask import Flask
 from datetime import datetime
+from MapCreator import MapCreator
 app = Flask(__name__)
 
 @app.route('/')
 def homepage():
+    map_creator = MapCreator()
+
+    map_creator.colony_loss_map()
     the_time = datetime.now().strftime("%A, %d %b %Y %l:%M %p")
 
     return """
